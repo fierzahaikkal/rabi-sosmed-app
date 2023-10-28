399c6db5a650d4030596fc6b4ae2486a270d3cc0
damfalah <m.adamfalah12@gmai.com>
2023-10-28 09:59:30 +0700

    feet: menamambahkan fitur pada topi section

--- src/components/topic-section.tsx
+++ src/components/topic-section.tsx
@@ -2,32 +2,32 @@ import { Plus } from 'lucide-react';
 export default function TopicSection() {
     return (
     <div className="w-full h-12 pb-2 border-b border-slate-950 justify-between items-center inline-flex">
-        <Plus />
+        <div className='p-2 hover:bg-neutral-300'><a href="/halaman-lain"><Plus/></a></div>
         <div className="w-6 h-6 relative" />
         <div className="justify-center items-center flex">
-            <div className="p-2 bg-zinc-200 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">For You</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">For You</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Sport</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Sport</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Foods</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Foods</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Nature</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Nature</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Technology</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Technology</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Programming</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Programming</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Networking</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Networking</a>
             </div>
-            <div className="p-2 justify-center items-center gap-2.5 flex">
-                <div className="text-right text-slate-950 text-body leading-normal">Drawing</div>
+            <div className="p-2 justify-center items-center gap-2.5 flex hover:bg-neutral-300">
+                <a href="/halaman-lain" className="text-right text-slate-950 text-body leading-normal">Drawing</a>
             </div>
         </div>
     </div>
