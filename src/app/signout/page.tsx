5adc0fc7ef895be40b22fafa8861cf0d8584a648
fierzahaikkal <ojogames.id@gmail.com>
2023-10-28 16:00:38 +0700

    feat: adding textarea

--- src/app/signout/page.tsx
+++ src/app/signout/page.tsx
@@ -1,3 +1,9 @@
+import InputSection from '@/components/input-section';
+
 export default function SignOut() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col items-center gap-y-[80px] px-[160px]">
+      <InputSection />
+    </main>
+  );
 }
