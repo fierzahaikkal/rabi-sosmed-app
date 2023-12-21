05b0e67f539665abdc93da0cb8c1abc2b737ce2f
Timoy <timotyjoel2811@gmail.com>
2023-12-22 00:17:23 +0700

    feat/topicPage: tambah varian btn + ubah btn ketika diklik

--- src/app/topics/page.tsx
+++ src/app/topics/page.tsx
@@ -14,69 +14,105 @@ type Topics = 'technology' | 'foods' | 'nature' | 'sport' | 'programming' | 'net
 
 function TopicPage() {
     const [topics, setTopics] = useState<Array<Topics>>([])
+    const [activeStates, setActiveStates] = useState<Array<boolean>>([false])
+    //const [isActive, setIsActive] = useState<boolean>(false)
+    let isActive = false
     console.log(topics)
+    console.log(activeStates)
 
-    function handleSelectTopic(topic: string): void {
+    function handleSelectTopic(index: number, topic: string): void {
+        //setIsActive(!isActive)
+        const newActiveStates = [...activeStates]
         switch(topic) {
             case 'sport':
                 if (topics.includes("sport")) {
                     const removedTopic = topics.filter(item => item !== 'sport');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false;
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "sport"])
+                    newActiveStates[index] = true;
+                    //setIsActive(!isActive)
                 }
                 break;
             case 'foods':
                 if (topics.includes("foods")) {
                     const removedTopic = topics.filter(item => item !== 'foods');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false;
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "foods"])
+                    newActiveStates[index] = true;
+                    //setIsActive(!isActive)
                 }
                 break;
             case 'nature':
                 if (topics.includes("nature")) {
                     const removedTopic = topics.filter(item => item !== 'nature');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "nature"])
+                    newActiveStates[index] = true;
+                    //setIsActive(true)
                 }
                 break;
             case 'programming':
                 if (topics.includes("programming")) {
                     const removedTopic = topics.filter(item => item !== 'programming');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "programming"])
+                    newActiveStates[index] = true
+                    //setIsActive(true)
                 }
                 break;
             case 'technology':
                 if (topics.includes("technology")) {
                     const removedTopic = topics.filter(item => item !== 'technology');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "technology"])
+                    newActiveStates[index] = true
+                    //setIsActive(true)
                 }
                 break;
             case 'networking':
                 if (topics.includes("networking")) {
                     const removedTopic = topics.filter(item => item !== 'networking');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "networking"])
+                    newActiveStates[index] = true
+                    //setIsActive(true)
                 }
                 break;
             case 'art':
                 if (topics.includes("art")) {
                     const removedTopic = topics.filter(item => item !== 'art');
                     setTopics(removedTopic)
+                    newActiveStates[index] = false
+                    //setIsActive(false)
                 } else {
                     setTopics([...topics, "art"])
+                    newActiveStates[index] = true
+                    //setIsActive(true)
                 }
                 break;
             default:
                 break;
         }
+
+        setActiveStates(newActiveStates);
     }
 
     return (
@@ -102,25 +138,25 @@ function TopicPage() {
                 <div 
                 style={{maxWidth: 400}}
                 className="flex flex-wrap gap-x-4 gap-y-3 max-w-3 w-full">
-                    <Button onClick={() =>handleSelectTopic("sport")} className="bg-indigo-600 text-white w-xl p-2">
+                    <Button variant={"topics"} onClick={() =>handleSelectTopic(0, "sport")} className={`text-white w-xl p-2 ${activeStates[0] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Sport
                     </Button>
-                    <Button onClick={() => handleSelectTopic('foods')} className="bg-purple-200 text-slate-950 w-xl p-2">
+                    <Button variant={"topics"} onClick={() => handleSelectTopic(1, 'foods')} className={`text-white w-xl p-2 ${activeStates[1] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Foods
                     </Button>
-                    <Button onClick={() => handleSelectTopic("nature")} className="bg-indigo-600 text-white w-xl p-2">
+                    <Button variant={"topics"} onClick={() => handleSelectTopic(2, "nature")} className={`text-white w-xl p-2 ${activeStates[2] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Nature
                     </Button>
-                    <Button onClick={() => handleSelectTopic("programming")} className="bg-indigo-600 text-white w-xl p-2 px-4 ">
+                    <Button variant={"topics"} onClick={() => handleSelectTopic(3, "programming")} className={`text-white w-xl p-2 ${activeStates[3] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Programming
                     </Button>
-                    <Button onClick={() => handleSelectTopic("technology")} className="bg-indigo-600 text-white w-xl p-2">
+                    <Button variant={"topics"} onClick={() => handleSelectTopic(4, "technology")} className={`text-white w-xl p-2 ${activeStates[4] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Technology
                     </Button>
-                    <Button onClick={() => handleSelectTopic("networking")} className="bg-purple-200 text-slate-950 w-xl p-2">
+                    <Button variant={"topics"} onClick={() => handleSelectTopic(5, "networking")} className={`text-white w-xl p-2 ${activeStates[5] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Networking
                     </Button>
-                    <Button onClick={() => handleSelectTopic("art")} className="bg-purple-200 text-slate-950 w-xl p-2">
+                    <Button variant={"topics"} onClick={() => handleSelectTopic(6, "art")} className={`text-white w-xl p-2 ${activeStates[6] ? 'bg-indigo-100 text-black': 'bg-indigo-600'}`}>
                         Art
                     </Button>
                 </div>
