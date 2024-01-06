41ba7f5f24f18eb1dd9e4fd547e3239846578452
damfalah <m.adamfalah12@gmai.com>
2024-01-06 11:27:10 +0700

    feat: menambahkan footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,49 @@
 import React from 'react';
+import { Github, Codepen, Youtube, Heart, Cookie } from 'lucide-react';
 
 const MyComponent: React.FC = () => {
   return (
-    <div className="footer" id="footer" data-astro-cid-sz7xmlte>
-      <style>
-        {`
-                .footer[data-astro-cid-sz7xmlte] {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: flex-start;
-                    gap: 2em;
-                    border-radius: 10px;
-                    background-color: var(--static8);
-                    color: var(--text);
-                    padding: 3em 5em;
-                    width: 100%;
-                    margin-bottom: 10em;
-                }
-                `}
-      </style>
-      <div className="logo" data-astro-cid-sz7xmlte>
-        <a href="#" data-astro-cid-sz7xmlte>
+    <div className="flex w-full flex-col items-start gap-8 rounded-lg bg-gray-200 p-12">
+      <div className="logo">
+        <a href="#">
           <h3 className="text-3xl font-semibold text-primary">Rabi</h3>
           <p className="text-xs text-accent">Rencana Bicara</p>
         </a>
       </div>
 
-      <div className="footer-cols" data-astro-cid-sz7xmlte>
-        <div className="footer-col" data-astro-cid-sz7xmlte>
-          <div className="reversed menu-item-header" data-astro-cid-sz7xmlte></div>
-          <div className="menu-item reversed" data-astro-cid-sz7xmlte>
-            <a href="mailto:youremail@example.com">Contact me</a>
-          </div>
-          <div className="menu-item reversed exclude" data-astro-cid-sz7xmlte>
-            <a href="https://www.youtube.com/yourchannel" target="_blank">
-              YouTube
-            </a>
-          </div>
-          <div className="menu-item reversed exclude" data-astro-cid-sz7xmlte>
-            <a href="https://github.com/yourusername" target="_blank">
-              GitHub
-            </a>
-          </div>
-          <div className="menu-item reversed exclude" data-astro-cid-sz7xmlte>
-            <a href="https://codepen.io/yourusername" target="_blank">
-              CodePen
-            </a>
-          </div>
+      <div className="footer">
+        <div className="reversed menu-item-header">LET'S CONTACT</div>
+        <div className="menu-item reversed">
+          <a href="mailto:youremail@example.com">Contact me</a>
         </div>
+        <div className="menu-item reversed exclude">
+          <a href="https://www.youtube.com" target="_blank" className="flex">
+            <Youtube className="ml-1 mr-1" />
+            YouTube
+          </a>
+        </div>
+        <div className="menu-item reversed exclude">
+          <a href="https://github.com" target="_blank" className="flex">
+            <Github className="ml-1 mr-1" />
+            GitHub
+          </a>
+        </div>
+        <div className="menu-item reversed exclude">
+          <a href="https://codepen.io" target="_blank" className="flex">
+            <Codepen className="ml-1 mr-1" />
+            CodePen
+          </a>
+        </div>
+      </div>
+      <hr className="w-full border-b-2 border-black" />
+      <div>
+        <p className="flex items-center">
+          Made with <Heart className="ml-1 mr-1" /> by Rabi
+        </p>
+        <p className="flex items-center">
+          No cookies <Cookie className="ml-1 mr-1" />
+        </p>
+        <p>copyright reserved 2023 @ pweb assignment</p>
       </div>
     </div>
   );
