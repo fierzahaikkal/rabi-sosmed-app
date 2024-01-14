7c1943b85083697a2de948be4c06873d55d31559
Frdyan <ferdiharyadi123@gmail.com>
2024-01-14 15:14:53 +0700

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-import CardBanner from "@/components/card-banner";
-import LandingHeader from "@/components/landing-header";
-import LicenseFooter from "@/components/license-footer";
-import Navhead from "@/components/navhead";
-import Image from "next/image";
-import gradient from "public/gradient.svg";
+import React from 'react';
+import NavBar from '@/components/NavBar'; 
 
-export default function Home() {
+export default function Feeds() {
   return (
-    <main className="relative flex flex-col gap-y-20 overflow-hidden px-[160px]">
-      <Image
-        src={gradient}
-        objectFit="cover"
-        fill
-        alt="background gradient"
-        className="absolute z-[-1] mx-auto"
-      />
-      <Navhead></Navhead>
-      <div className="flex flex-col items-center">
-        <LandingHeader></LandingHeader>
-      </div>
-      <CardBanner></CardBanner>
-      <LicenseFooter />
+    <main>
+      <NavBar />
     </main>
   );
 }
